b6e38fd4ce271ce0782e97615caa45ad8d0d3569
Anton <enitefall81@gmail.com>
2021-05-20 11:15:46 -0700

    UseEffect check

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-function App() {
-  function setInterval() {
-    
-  }
-  
-  return <div>
+import {useEffect, useState} from "react";
 
+function App() {
+
+  let [counter, setCounter] = useState(0)
+
+  useEffect(_=> {
+    document.title = "Anton`s App"
+    console.log("applied")
+  }, [])
+
+  return <div>
+<button onClick={_ => {setCounter(counter - 1 )}}> - </button>
+    {" "}
+    {counter}
+<button onClick={_ => {setCounter(counter + 1 )}}> + </button>
   </div>
 }
 
