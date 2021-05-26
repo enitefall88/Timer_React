045f62d95a41ed1f030a52aec1494908f69642b2
Anton <enitefall81@gmail.com>
2021-05-25 18:36:30 -0700

    Add App_custom with Objects

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App_custom from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App_custom />
   </React.StrictMode>,
   document.getElementById('root')
 );
